353a76d0ff68526e87c8fa487e08c0a8d8de7b4d
glizzygobblrr <S10257120@connect.np.edu.sg>
2024-10-29 11:27:41 +0800

    update dbConfig for RDS connection

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -3,11 +3,12 @@ require('dotenv').config();
 module.exports = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
+  server: process.env.DB.SERVER,
   database: process.env.DB_DATABASE,
   port: 1433, 
   connectionTimeout: 60000, 
   options: {
     trustServerCertificate: true,
+    enableArithAbort: true,
   },
 };
